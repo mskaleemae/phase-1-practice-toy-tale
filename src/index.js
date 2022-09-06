90be5a5ec69546b11079cb593a9e693e4fb974d3
Kaeli Gilmore <mskaleemae2000@gmail.com>
2022-09-06 11:50:39 -0400

    commit tree 7946184037cc37229e4222146a20de11391b514e

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 let addToy = false;
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
+let divCollect = document.querySelector('#toy-collection')
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +16,48 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function getToys() {
+  return fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+}
+
+function postToy(toy_data) {
+  fetch('http://localhost:3000/toys',{
+    method: 'POST',
+      headers: {
+      'Content-Type': 'application/json',
+        Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      "name": toy_data.name.value,
+      "image": toy_data.image.value,
+      "likes":0
+    })
+  })
+    .then(res => res.json())
+    .then((obj_toy) => {
+      let new_toy = renderToys(obj_toy)
+      divCollect.append(new_toy)
+  })
+}
+
+function likes(e) {
+  e.preventDefault()
+  let more = parseInt(e.target.previousElementSibling.innerText)+1
+
+  fetch('http://localhost:3000/toys', {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      "likes": more
+    })
+  })
+    .then(res => res.json())
+    .then((like_obj => {
+      e.target.previousElementSibling.innerText = '${more}likes';
+  }))
+}
